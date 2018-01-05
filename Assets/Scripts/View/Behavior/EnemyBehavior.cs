021b8fcf1cc2b330add2165028238f76fb57e34d
4419-3-6\user <wanan850405@gmail.com>
2018-01-05 20:48:26 +0800

    no message

---
+++ Assets/Scripts/View/Behavior/EnemyBehavior.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(HealthComponent))]
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(MeshFader))]
+[RequireComponent(typeof(Animator))]//attribute
+public class EnemyBehavior : MonoBehaviour {
+    private Animator animator;
+    private MeshFader meshFader;
+    private AudioSource audioSource;
+    private HealthComponent healthComponent;
+    [SerializeField]
+    private AudioClip hurtClip;
+    [SerializeField]
+    private AudioClip deadClip;
+    public EnemyData enemyData;
+    public bool IsDead
+    {
+        get
+        {
+            return healthComponent.IsOver;
+        }
+    }
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        meshFader = GetComponent<MeshFader>();
+        audioSource = GetComponent<AudioSource>();
+        healthComponent = GetComponent<HealthComponent>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(meshFader.FadeIn());
+    }
+
+    [ContextMenu("Test Execute")]
+    private void TestExecute()
+    {
+        StartCoroutine(Execute(enemyData));
+    }
+
+    public IEnumerator Execute(EnemyData enemyData)
+    {
+        healthComponent.Init(enemyData.health);
+        while(IsDead == false)
+        {
+            yield return null;
+        }
+        animator.SetTrigger("die");
+        audioSource.clip = deadClip;
+        audioSource.Play();
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+        yield return StartCoroutine(meshFader.FadeOut());
+    }
+
+    public void DoDamage(int attack)
+    {
+        animator.SetTrigger("hurt");
+        audioSource.clip = hurtClip;
+        audioSource.Play();
+        healthComponent.Hurt(attack);
+    }
+
+    private void Update()
+    {
+        if (healthComponent.IsOver)
+            return;
+        if(Input.GetButtonDown("Fire1"))
+        {
+            DoDamage(10);
+        }
+    }
+}
